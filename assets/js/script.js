9a80782c654dcf9ff0d1b914d57021aff022b4b4
Brina <sabrinazanin97@gmail.com>
2021-12-17 16:01:18 -0500

    added timer func

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var timerEl = document.getElementById('countdown');
+var startBtn = document.querySelector("#start");
+
+var countdown = function() {
+    var timeLeft = 75;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 0) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = 'Time: ' + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+        timerEl.textContent = 'Time: 0';
+    }
+  }, 1000);
+}
+
+function startquiz() {
+    countdown();
+};
+
+
+startBtn.addEventListener("click", startquiz);
